eb41c13fdf0268313070b8c9bdf0b976c5e96b66
Dinesh <Dinesh01@gmail.com>
2025-11-19 16:26:29 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello, world!"
+
+@app.route("/watsapp", methods=['POST'])
+def sms_reply():
+    """"Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+
+    # create reply
+    resp = MessagingResponse()
+    resp.message("dk")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+ 
